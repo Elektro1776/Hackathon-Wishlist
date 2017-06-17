1736358791c03c2d28ce9acbd3abebefc09a6c7f
Austin <elektricwebdesign@gmail.com>
2017-06-17 13:43:17 -0600

    added some comments to the app.js file

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// How do we start our javascript files ??
+
+// What features will we want the most basic version of this app to have
+
+
+// what things do we know we will need to keep track of right away ?
+
+
+// what "User" are we writting  functions for
+
+
+// what Are the different types of "Users" ?
+
+
+// Seperate our presentational logic from business logic
+
+// Sketch out flow diagram of user interaction with the website
